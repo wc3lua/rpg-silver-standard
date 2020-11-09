4a11b0ab63bde4427be8868144892f52eeaeadc0
WriteCoin <voronckov.danila@yandex.ru>
2020-11-09 03:14:16 +0300

    раскомментирован

--- runconfig.lua
+++ runconfig.lua
@@ -1,9 +1,9 @@
 -- This is how you tell Ceres how to launch your game.
 -- Without this, `ceres run` will not work.
 -- Uncomment and fill in the fields that you want
--- ceres.runConfig = {
---     command = "path/to/warcraft iii.exe", -- mandatory, path to WC3 or a shell script
---     prefix = "", -- this will be prepended to the map path when launching. useful under Wine. leave this commented out on Windows.
---     mapDataDir = "/path/to/Documents/Warcraft III/CustomMapData", -- points Ceres to the CustomMapData folder for interop with the WC3 client. optional.
---     args = {"-windowmode", "windowed"} -- extra args if you want to specify any.
--- }
+ceres.runConfig = {
+    command = "C:\\Games\\Warcraft III Public Test\\x86_64\\Warcraft III.exe", -- mandatory, path to WC3 or a shell script
+    -- prefix = "", -- this will be prepended to the map path when launching. useful under Wine. leave this commented out on Windows.
+    -- mapDataDir = "/path/to/Documents/Warcraft III/CustomMapData", -- points Ceres to the CustomMapData folder for interop with the WC3 client. optional.
+    args = {"-windowmode", "windowed"} -- extra args if you want to specify any.
+}
