45f298fc372d16e89efa5f2ce5dd32d1dc77f23b
WriteCoin <voronckov.danila@yandex.ru>
2021-11-03 16:25:46 +0300

    изменены подключаемые модули

--- src/index.lua
+++ src/index.lua
@@ -1,11 +1,1 @@
-require 'modules.other-functions.index'
-require 'modules.native-types.index'
-require 'modules.lua-lib.index'
-
-local h = Handle.get(GetEnumUnit())
-
-print(getType(h))
-
-print(h.handle)
-
-print(Handle.getId(h))
+print('lua')
